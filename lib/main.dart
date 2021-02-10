6e06f320a181afedfba69ce3ac8ed8170d678257
Vladyslav_Patsay@epam.com <VP0310vp1108>
2021-02-10 12:25:19 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Test App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Test App Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  static final Random _random = Random();
+
+  static Color randomColor() {
+    return Color.fromARGB(
+      _random.nextInt(256),
+      _random.nextInt(256),
+      _random.nextInt(256),
+      _random.nextInt(256),
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: GestureDetector(
+          child: Scaffold(
+            backgroundColor: randomColor(),
+            appBar: AppBar(
+              title: Text(widget.title),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Hey there',
+                  ),
+                ],
+              ),
+            ),
+          ),
+          onTap: () => setState(() => {}),
+        )
+    );
+  }
+}
